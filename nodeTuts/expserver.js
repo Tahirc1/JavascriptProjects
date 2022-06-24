efa96ae73090a9d6e10a4a53b6f460bed7643f73
tahir <chaudharitahir1@gmail.com>
2022-06-24 20:31:24 +0530

    express server completed

--- nodeTuts/expserver.js
+++ nodeTuts/expserver.js
@@ -4,11 +4,16 @@ var app = express();
 app.set('view engine','ejs')
 app.set('views', path.join(__dirname,'views'))
 app.use('/assets',express.static('assets'))
+app.use(express.urlencoded({extended:true}))
 app.listen(3000);
 app.get('/',(req,res)=>{res.send('this is a response')})
 app.get('/contact',(req,res)=>{
     console.log(req.query)
     res.render('contact',{que : req.query})
+})
+app.post('/contact',(req,res)=>{
+    console.log(req.body);
+    res.render('contact-post',{data: req.body})
 })
 app.get('/profile/:id',(req,res)=>{
     let data = {name: 'jason' , hobbies : ['art' , 'dance' , 'sing']} 
